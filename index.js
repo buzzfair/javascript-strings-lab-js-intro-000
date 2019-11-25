dbf9e9820fb615d7151cc01f6dc7f18bb1b44879
Guin White <guin.white@gmail.com>
2019-11-25 22:43:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = 'Hello, '+ specialGuest + '!';
 
+<<<<<<< HEAD
 var topic = "Let's talk about Let's talk about space..";
+=======
+var topic = 'Let\'s talk about space';
+>>>>>>> bf9e87cae782dd0344410d069c71cb9489a025e9
 
 var conversation = `${topic}`
